d03e0c2033195441cf00cb1e7057fbd05578c210
PhatLee <vanphat15it@gmail.com>
2025-05-06 08:05:08 +0700

    feat(docker): update Docker configuration for MySQL and add Certbot service

--- src/main/java/com/group/docorofile/DoCoroFileApplication.java
+++ src/main/java/com/group/docorofile/DoCoroFileApplication.java
@@ -5,11 +5,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.PropertySource;
 
+import java.util.TimeZone;
+
 @SpringBootApplication
 @PropertySource(value = "classpath:.env", ignoreResourceNotFound = true)
 public class DoCoroFileApplication {
-
     public static void main(String[] args) {
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Ho_Chi_Minh"));
         SpringApplication.run(DoCoroFileApplication.class, args);
     }
 
