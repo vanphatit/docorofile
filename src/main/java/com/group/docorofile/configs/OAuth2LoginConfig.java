7ab31ea9091fda15f6563f0d1de222dd1758946f
PhatLee <vanphat15it@gmail.com>
2025-09-04 10:33:07 +0700

    feat(api): update API endpoints to use local server URLs

--- src/main/java/com/group/docorofile/configs/OAuth2LoginConfig.java
+++ src/main/java/com/group/docorofile/configs/OAuth2LoginConfig.java
@@ -30,7 +30,7 @@ public class OAuth2LoginConfig {
                 .clientSecret(GOOGLE_CLIENT_SECRET)
                 .clientAuthenticationMethod(ClientAuthenticationMethod.CLIENT_SECRET_BASIC)
                 .authorizationGrantType(AuthorizationGrantType.AUTHORIZATION_CODE)
-                .redirectUri("https://docorofile.phatit.id.vn/login/oauth2/code/{registrationId}")
+                .redirectUri("http://localhost:9091/login/oauth2/code/{registrationId}")
                 .scope("openid", "profile", "email", "address", "phone")
                 .authorizationUri("https://accounts.google.com/o/oauth2/v2/auth")
                 .tokenUri("https://www.googleapis.com/oauth2/v4/token")
